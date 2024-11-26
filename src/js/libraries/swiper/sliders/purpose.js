b279b61a90b01760fe05da98ad39194bd1a78177
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-11-27 00:41:36 +0400

    Fix purposes slider

--- src/js/libraries/swiper/sliders/purpose.js
+++ src/js/libraries/swiper/sliders/purpose.js
@@ -14,10 +14,10 @@ if (purposes && sliders.length) {
   const prev = purposes.querySelector(".purposes-arrows__button--prev");
   const next = purposes.querySelector(".purposes-arrows__button--next");
 
-  /** @type {Swiper} */
-  let swiper;
-
   sliders.forEach((slider) => {
+    /** @type {Swiper} */
+    let swiper;
+
     initSlider(slider);
 
     MIN_WIDTH_768_PX.addEventListener("change", (event) => {
@@ -25,6 +25,42 @@ if (purposes && sliders.length) {
 
       initSlider(slider);
     });
+
+    /** @param {HTMLDivElement} slider */
+    function initSlider(slider) {
+      setTimeout(() => {
+        swiper = new Swiper(slider, {
+          modules: [Keyboard, Navigation,],
+          keyboard: {
+            enabled: true,
+            pageUpDown: false,
+          },
+          navigation: {
+            enabled: true,
+            nextEl: next,
+            prevEl: prev,
+          },
+          on: {
+            init: (swiper) => {
+              const { navigation } = swiper;
+              const { prevEl, nextEl } = navigation;
+              const { matches } = MIN_WIDTH_768_PX;
+
+              if (!matches) {
+                prevEl.classList.add("swiper-button-disabled");
+              }
+
+              setClassToSlider(swiper);
+              activateNextButton(swiper);
+            },
+            resize: setClassToSlider,
+          },
+          slidesPerView: "auto",
+          spaceBetween: 8,
+          allowTouchMove: false,
+        });
+      });
+    }
   });
 
   purposeItems.forEach((purposeItem) => {
@@ -40,33 +76,46 @@ if (purposes && sliders.length) {
     }
   });
 
-  /** @param {HTMLDivElement} slider */
-  function initSlider(slider) {
-    swiper = new Swiper(slider, {
-      modules: [Keyboard, Navigation,],
-      keyboard: {
-        enabled: true,
-        pageUpDown: false,
-      },
-      navigation: {
-        enabled: true,
-        nextEl: next,
-        prevEl: prev,
-      },
-      on: {
-        init: (swiper) => {
-          const { navigation } = swiper;
-          const { prevEl } = navigation;
-          const { matches } = MIN_WIDTH_768_PX;
+  /** @param {Swiper} swiper */
+  function setClassToSlider(swiper) {
+    const { el, wrapperEl } = swiper;
+    const { width: elWidth } = el.getBoundingClientRect();
+    const { scrollWidth: wrapperWidth } = wrapperEl;
 
-          if (!matches) {
-            prevEl.classList.add("swiper-button-disabled");
-          }
-        },
-      },
-      slidesPerView: "auto",
-      spaceBetween: 8,
-      allowTouchMove: false,
+    el.classList.toggle("purpose-slider--layer", elWidth < wrapperWidth);
+  }
+
+  /** @param {Swiper} swiper */
+  function activateNextButton(swiper) {
+    setTimeout(() => {
+      const { navigation } = swiper;
+
+      if (navigation) {
+        const { nextEl } = navigation;
+
+        const isNextElActive = [...sliders].some(
+          /** @param {HTMLDivElement} slider */
+          (slider) => {
+            /** @type {Swiper} */
+            const { swiper } = slider;
+
+            if (swiper) {
+              const { el, wrapperEl } = swiper;
+              const { width: elWidth } = el.getBoundingClientRect();
+              const { scrollWidth: wrapperWidth } = wrapperEl;
+
+              if (elWidth < wrapperWidth) {
+                return true;
+              }
+            }
+
+            return false;
+          });
+
+        if (isNextElActive) {
+          nextEl.classList.remove("swiper-button-disabled");
+        }
+      }
     });
   }
 }
