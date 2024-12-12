caf435187008d31c2bf6130b9da19ef19ce4206a
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2024-12-12 11:27:42 +0400

    Added certificates section

---
+++ src/js/libraries/swiper/sliders/certificates.js
@@ -0,0 +1,40 @@
+import Swiper from "swiper";
+import { Keyboard, Navigation, Pagination, } from "swiper/modules";
+
+const rewardsSlider = document.querySelector(".rewards-slider");
+
+if (rewardsSlider) {
+  const pagination = rewardsSlider.querySelector(".slider-pagination");
+  const prev = rewardsSlider.querySelector(".slider-arrows__button--prev");
+  const next = rewardsSlider.querySelector(".slider-arrows__button--next");
+
+  const swiper = new Swiper(rewardsSlider, {
+    modules: [Keyboard, Navigation, Pagination,],
+    keyboard: {
+      enabled: true,
+      pageUpDown: false,
+    },
+    navigation: {
+      enabled: true,
+      nextEl: next,
+      prevEl: prev,
+    },
+    pagination: {
+      clickable: true,
+      el: pagination,
+      enabled: true,
+    },
+    breakpoints: {
+      "550.1": {
+        slidesPerView: 3,
+        spaceBetween: 16,
+      },
+      "992.1": {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+    slidesPerView: 2.11875,
+    spaceBetween: 8,
+  });
+}
--- src/js/libraries/swiper/swiper.js
+++ src/js/libraries/swiper/swiper.js
@@ -1,3 +1,4 @@
 import "./sliders/product.js";
 import "./sliders/purpose.js";
 import "./sliders/objects.js";
+import "./sliders/certificates.js";
