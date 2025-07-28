5a12e55ac97c76278670456da37bce96398d9914
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2025-07-28 05:59:17 +0400

    Refactor code structure for improved readability and maintainability

--- src/js/libraries/swiper/sliders/product-card.js
+++ src/js/libraries/swiper/sliders/product-card.js
@@ -1,5 +1,5 @@
 import Swiper from "swiper";
-import { Pagination, } from "swiper/modules";
+import { Pagination, Mousewheel, } from "swiper/modules";
 
 /** @type {NodeListOf<HTMLDivElement>} */
 const productCardSliders = document.querySelectorAll(".product-card-slider");
@@ -9,10 +9,13 @@ productCardSliders.forEach((productCardSlider) => {
   const pagination = productCardSlider.querySelector(".slider-pagination");
 
   const swiper = new Swiper(productCardSlider, {
-    modules: [Pagination,],
+    modules: [Pagination, Mousewheel,],
     pagination: {
       clickable: true,
       el: pagination,
+      enabled: true,
+    },
+    mousewheel: {
       enabled: true,
     },
     spaceBetween: 10,
